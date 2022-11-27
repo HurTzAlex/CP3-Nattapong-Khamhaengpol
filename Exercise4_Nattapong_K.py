60d64b1e1465a932a8a4886e3b6a293ead5eeaf1
HurTzAlex <nattap0927@gmail.com>
2022-11-27 19:59:50 +0700

    Create Exercise4

---
+++ Exercise4_Nattapong_K.py
@@ -0,0 +1,9 @@
+ENG = 88
+BUS = 92
+CSS = 97.2
+CPG = 98.9
+print("--- Your Score ---")
+print("Foundation English :",ENG)
+print("General Business :",BUS)
+print("Introduction to Computer System :",CSS)
+print("Computer Programming :",CPG)
